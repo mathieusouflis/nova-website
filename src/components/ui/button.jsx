c1199a0cfb050383c5e55e34bdfce5ac929dd3c5
Mathieu <spiie.pro@souflis.fr>
2024-08-28 20:12:20 +0200

    Styles for sub button

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -22,8 +22,9 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
+        sm: "h-9 rounded-md px-3",
+        xs: "h-8 rounded-md px-3",
         icon: "h-10 w-10",
       },
     },
