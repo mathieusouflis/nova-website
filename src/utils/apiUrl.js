8a1e8a52631cf39af9b08790d136daa78c8b38cd
Mathieu <spiie.pro@souflis.fr>
2024-09-01 14:18:39 +0200

    Url...

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,3 +1,3 @@
-const apiURL = "http://localhost:3001/api";
-// const apiURL = "https://nova-api-s2m2r.ondigitalocean.app/api";
+// const apiURL = "http://localhost:3001/api";
+const apiURL = "https://nova-api-s2m2r.ondigitalocean.app/api";
 export default apiURL;
