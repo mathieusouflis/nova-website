1d19a5126e3bf55d3a5d87a550d82e9d0c7a49eb
Mathieu <spiie.pro@souflis.fr>
2024-09-02 14:56:50 +0200

    Make links thiner

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -5,19 +5,20 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-base ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-base text-primary-foreground hover:bg-primary/90",
+          " font-medium bg-primary text-base text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-base text-destructive-foreground hover:bg-destructive/90",
+          " font-medium bg-destructive text-base text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border text-base border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          " font-medium border text-base border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-base text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent text-base hover:text-accent-foreground",
+          " font-medium bg-secondary text-base text-secondary-foreground hover:bg-secondary/80",
+        ghost:
+          " font-medium hover:bg-accent text-base hover:text-accent-foreground",
         link: "text-primary text-base underline-offset-4 hover:underline",
       },
       size: {
