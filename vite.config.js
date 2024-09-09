180937d963d668e53bde8ed31e1fa4e39ac49447
Mathieu <spiie.pro@souflis.fr>
2024-09-09 11:08:40 +0200

    ssl enabled in dev env

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
