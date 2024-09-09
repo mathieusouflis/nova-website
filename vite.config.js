e55d9e777c97a479ceba76689eb9922f88a9baa5
Mathieu <spiie.pro@souflis.fr>
2024-09-09 14:16:00 +0200

    Vite config for prod

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,19 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
+// DEV
+// export default defineConfig({
+//   plugins: [react(),  basicSsl()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+// PROD
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
