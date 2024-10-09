1e1d861950afee830a879d4595eee9d6cfa1e888
Mathieu <spiie.pro@souflis.fr>
2024-10-09 20:13:15 +0200

    Api Url Change

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,21 @@ import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // DEV
-// export default defineConfig({
-//   plugins: [react(),  basicSsl()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// });
-
-// PROD
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
+// PROD
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
